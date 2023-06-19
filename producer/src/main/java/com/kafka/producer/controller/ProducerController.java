e4bb7c503ea009c670bf39215bc74246397f4013
Shiv(Workspace) <shivatneosoft@gmail.com>
2023-06-19 12:50:34 +0530

    Correcting Controller and application.

--- producer/src/main/java/com/kafka/producer/controller/ProducerController.java
+++ producer/src/main/java/com/kafka/producer/controller/ProducerController.java
@@ -1,8 +1,10 @@
 package com.kafka.producer.controller;
 
 import com.kafka.producer.service.ProducerService;
+import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +16,7 @@ public class ProducerController {
     private ProducerService producerService;
 
     @GetMapping("/producer")
-    public String sendMessage(String message){
+    public String sendMessage(@PathParam("message") String message){
         return this.producerService.sendMessage(message);
     }
 
